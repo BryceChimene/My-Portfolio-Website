e8975aced07fee28530463d9628106e1ce735ac3
Bryce Chimene <brycechimene@yahoo.com>
2025-06-26 14:27:27 -0500

    Create particle background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,14 @@ import "../App.css";
 import { ToggleTheme } from "./toggle_theme/ToggleTheme";
 import Hero from "./hero/Hero";
 
+import ParticlesComponent from './particles';
+
 export default function App() {
   const [isDark, setIsDark] = useLocalStorage("isDark", false);
 
   return (
     <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <ParticlesComponent id="particles" isDark={isDark}/>
       <Hero /> 
       <ToggleTheme isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
     </div>
